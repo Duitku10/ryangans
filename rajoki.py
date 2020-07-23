03ca17824481817062d9fb8412c9268663d47022
Duitku10 <68714718+Duitku10@users.noreply.github.com>
2020-07-24 05:34:55 +0700

    Add files via upload

---
+++ rajoki.py
@@ -0,0 +1,62 @@
+import os
+import sys
+import time
+
+os.system('clear')
+time.sleep(1)
+os.system('figlet Hack FB')
+print "======================================"
+print " Author : Sanz"
+print " Youtube: SANZ SOEKAMTI"
+print " Github : https://github.com/B4N954N2-ID"
+print "======================================"
+print
+time.sleep(1)
+print "[+] Menu Pilihan [+]"
+print "[1] Dark Fbv1.7"
+print "[2] Dark Fbv2.8"
+print "[3] Dark Fb Premium"
+print "[4] Dark Fbv2.0"
+print "[5] Install Bahan"
+time.sleep(1)
+pilih = raw_input('[?] Pilih : ')
+if pilih == "1":
+        os.system('git clone https://github.com/B4N954N2-ID/Dark')
+        time.sleep(1)
+        print "[+] Penginstallan Selesai"
+        time.sleep(1)
+        print "[+] Selanjutnya ketik $ cd Dark"
+        time.sleep(1)
+        print "[+] Lalu ketik $ python2 darkfb.py"
+elif pilih =="2":
+        os.system('git clone https://github.com/B4N954N2-ID/DarkFb')
+        time.sleep(1)
+        print "[+] Penginstallan Selesai"
+        time.sleep(1)
+        print "[+] Selanjutnya ketik $ cd DarkFb"
+        time.sleep(1)
+        print "[+] Lalu ketik $ python2 DarkFb.py"
+elif pilih == "3":
+        os.system('git clone https://github.com/B4N954N2-ID/DarkPremium')
+        time.sleep(1)
+        print "[+] Penginstallan Selesai"
+        time.sleep(1)
+        print "[+] Selanjutnya ketik $ cd DarkPremium"
+        time.sleep(1)
+        print "[+] Lalu ketik $ python2 darkpremium.py"
+elif pilih == "4":
+        os.system('git clone https://github.com/B4N954N2-ID/dark-fb')
+        time.sleep(1)
+        print "[+] Penginstallan Selesai"
+        time.sleep(1)
+        print "[+] Selanjutnya ketik $ cd dark-fb"
+        time.sleep(1)
+        print "[+] Lalu ketik $ python2 darkfb.py"
+elif pilih == "5":
+        os.system('apt update && apt upgrade')
+        os.system('pkg install python2')
+        os.system('pkg install nano')
+        os.system('pkg install git')
+        os.system('pip2 install requests mechanize')
+        time.sleep(1)
+        print "[+] Penginstallan Selesai"
